5f526dd041ecf2353e5b4c5eaf959bc00d2a3539
unknown <itsdawood100@gmail.com>
2024-09-06 00:29:52 +0500

    Updated login feature with backend integration

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import './Login.css';
-//import backgroundImage from '../assets/bg.png';  
 import logo from '../assets/logo2.png';  
 import { Link } from 'react-router-dom'; 
 
@@ -9,20 +8,41 @@ const Login = () => {
     const [password, setPassword] = useState('');
     const [error, setError] = useState('');
 
-    const handleSubmit = (e) => {
+    // Function to  send data to the backend
+    const handleSubmit = async (e) => {
         e.preventDefault();
- 
-        if (email !== 'test@example.com' || password !== 'password123') {
-            setError('Incorrect email or password');
-        } else {
-            setError('');
-            
-            alert('Login successful!');
+
+        const payload = {
+            email,
+            password
+        };
+
+        try {
+            // Sending a POST request to the backend
+            const response = await fetch('http://localhost:5000/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(payload), 
+            });
+
+            const data = await response.json();
+
+            if (response.ok) {
+                setError(''); 
+                alert('Login successful!');
+            } else {
+                setError(data.message); 
+            }
+        } catch (error) {
+            console.error('Error logging in:', error);
+            setError('An error occurred. Please try again.'); 
         }
     };
 
     return (
-        <div className="login-container" >
+        <div className="login-container">
             <div className="navbar">
                 <img src={logo} alt="E-bank" className="logo" />
                 <div className="nav-buttons">
