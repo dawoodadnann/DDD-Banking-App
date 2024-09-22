60d1c33a94e38be036280b5ab3c1438471975577
daniyal <daniyal236fahim@gmail.com>
2024-09-22 22:10:41 +0500

    Working Project AFTER ADDING BACKEND LOGIC OF BILLING AND MAKING DASHBOARD DYNAMIC

--- src/Components/Dashboard/Topbar.jsx
+++ src/Components/Dashboard/Topbar.jsx
@@ -1,11 +1,41 @@
+import { useState,useEffect } from "react";
 import React from "react";
 
 export const TopBar = () => {
+   const [email,setEmail]=useState(null);
+
+  useEffect(() => {
+    const fetchemail = async () => {
+      try {
+        // Sending a POST request to the backend
+        const response = await fetch("http://localhost:5000/getemail", {
+          method: "GET",
+          credentials: "include",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
+        const data = await response.json();
+
+      if (data.email) {
+          setEmail(data.email);
+      }else {
+          console.log(data.message);
+        }
+      } catch (error) {
+        console.error("Error retrieving balance:", error);
+        console.log("An error occurred. Please try again.");
+      }
+    };
+
+    fetchemail();
+  }, []); 
   return (
     <div className="border-b px-4 mb-4 mt-2 pb-4 border-stone-200">
       <div className="flex items-center justify-between p-0.5">
         <div>
-          <span className="text-sm font-bold block">🚀 Good morning, Dawood!</span>
+          <span className="text-sm font-bold block">🚀 Good morning, {email}</span>
           <span className="text-xs block text-stone-500">
             Tuesday, Aug 8th 2023
           </span>
